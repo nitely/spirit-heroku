c0fd7d982f2745dc0d2b88c9ec870f7d3af32014
nitely <ecastroborsani@gmail.com>
2016-03-26 11:07:00 -0300

    env vars

--- project/settings/heroku_prod.py
+++ project/settings/heroku_prod.py
@@ -43,7 +43,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', "")
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 
 # Email credentials
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST', "")
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', "")
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', "")
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', "")
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
